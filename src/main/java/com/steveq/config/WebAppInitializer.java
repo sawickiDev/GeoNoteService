00a7f4dc1ac2a6053b36dd629c5009942d7e9a1d
Adam Sawicki <adam.sawicki172@gmail.com>
2018-02-07 00:58:15 +0100

    basic rest config

---
+++ src/main/java/com/steveq/config/WebAppInitializer.java
@@ -0,0 +1,27 @@
+package com.steveq.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer{
+
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        AnnotationConfigWebApplicationContext dispatcherServlet =
+                new AnnotationConfigWebApplicationContext();
+        dispatcherServlet.register(AppConfig.class);
+
+        ServletRegistration.Dynamic servlet =
+                servletContext.addServlet("dispatcher", new DispatcherServlet(dispatcherServlet));
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+
+}
